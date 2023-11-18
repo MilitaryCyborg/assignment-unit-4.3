b84cbad4b5e839d7dbe59beaf90a874c3a99b657
Dean McNevin <deaner1456@gmail.com>
2023-11-18 10:22:27 -0600

    Finished 4.3

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,13 +2,27 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
+
+function addItem(item) {
+    basket.push(item);
+    return true;
+}
+console.log(`Basket is ${basket}`);
+console.log('Adding item ', addItem('item'));
+console.log(`What is in the basket?: ${basket}`);
+
+function listItems() {
+    for (let i = 0; i < basket.length; i++) {
+      console.log(basket[i]);
+    }
+  }
 
 
-
-
-
-
-
+  function empty() {
+    basket.length = 0;
+  }
+  console.log('Basket is now:', empty('empty'));
 
 
 
